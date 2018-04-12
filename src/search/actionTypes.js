c095895a6defa046290b63c9d8a177a7db05b709
Enzo Bruni <contacto@enzobruni.com>
2018-04-12 12:13:39 -0300

    Changes on Constants, added filterTypes

--- src/search/actionTypes.js
+++ src/search/actionTypes.js
@@ -3,3 +3,10 @@ export const fetchingTypes = {
   FETCHING_SUCCESS: "players/FETCHING_SUCCESS",
   FETCHING_ERROR: "players/FETCHING_ERROR"
 };
+
+export const filterTypes = {
+  SEARCH_KEYWORDS: "filters/SEARCH_KEYWORDS",
+  SEARCH_AGE: "filters/SEARCH_AGE",
+  SEARCH_POSITION: "filters/SEARCH_POSITION",
+  RESET_FILTERS: "filters/RESET_FILTERS"
+};
--- src/search/constants.js
+++ src/search/constants.js
@@ -1,10 +1,8 @@
-export const PLAYERS = 'https://football-players-b31f2.firebaseio.com/players.json';
-
+export const PLAYERS_URL = 'https://football-players-b31f2.firebaseio.com/players.json';
 export const positionsTypes = [
   'Attacking Midfield',
   'Central Midfield',
   'Centre-Back',
-  'Centre-Forward',
   'Centre-Forward',
   'Defensive Midfield',
   'Keeper',
